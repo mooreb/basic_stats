606a8fc41cc474e58fceebb51febaaad282538f8
Brian Moore <mooreb@mooreb.com>
2020-08-17 11:11:41 -0700

    multi map where the values are unique (sets instead of lists)

---
+++ raw/MultiMapUV.java
@@ -0,0 +1,37 @@
+package com.mooreb.util;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MultiMapUV<K,V>  {
+    private final Map<K, Set<V>> internalMap = new HashMap<>();
+
+    public void put(K k, V v) {
+        Set<V> set = internalMap.get(k);
+        if(null == set) {
+            set = new HashSet<V>();
+            internalMap.put(k, set);
+        }
+        set.add(v);
+    }
+
+    public Set<V> get(K k) {
+        return internalMap.get(k);
+    }
+
+    public Set<K> keySet() {
+        return internalMap.keySet();
+    }
+
+    public Set<Map.Entry<K,Set<V>>> entries() {
+        return internalMap.entrySet();
+    }
+
+    public Collection<Set<V>> values() {
+        return internalMap.values();
+    }
+
+}
