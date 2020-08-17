4ef7a40c67a32bb4209ff6e82a2fd6a0ff30c08b
Brian Moore <mooreb@mooreb.com>
2020-08-17 11:08:39 -0700

    wrap apache csv

---
+++ raw/CSVDataSource.java
@@ -0,0 +1,25 @@
+package com.mooreb.util;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.List;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class CSVDataSource {
+    private final File file;
+    public CSVDataSource(final File file) {
+        this.file = file;
+    }
+
+    public List<CSVRecord> getCSVRecords() throws IOException {
+        final Reader in = new FileReader(file);
+        final CSVParser parser = new CSVParser(in, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+        List<CSVRecord> list = parser.getRecords();
+        return Collections.unmodifiableList(list);
+    }
+}
